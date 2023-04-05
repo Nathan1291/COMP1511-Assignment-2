09a302d2eb4c9e7e1cab1ae37e9c2b7d858a37b1
z5477755 <z5477755@vx05>
2023-04-05 16:02:24 +1000

    Completed stage 1.1

---
+++ README.md
@@ -0,0 +1,2 @@
+# CS1511-Assignment 2
+A train carriage simulator made as part of the UNSW COMP1511 course. 
---
+++ cs_cs.c
@@ -0,0 +1,430 @@
+// Program name
+//
+// This program was written by [your name] (z5555555)
+// on [date]
+//
+// TODO: Description of program
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+///////////////////////////      Contants       ////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+// Provided constants
+#define ID_SIZE 6
+
+// Provided enums
+enum carriage_type {INVALID_TYPE, PASSENGER, BUFFET, RESTROOM, FIRST_CLASS};
+
+// TODO: Your #defines/enums can go here:
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+/////////////////////////// USER DEFINED TYPES  ////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+// A Train Carriage
+struct carriage {
+    // carriage id in the form #"N1002", unique, null terminated
+    char carriage_id[ID_SIZE];
+    //  Type of the carriage
+    enum carriage_type type;
+    // Maximum number of passengers 
+    int capacity;
+    // Current number of passengers
+    int occupancy;
+
+    struct carriage *next;
+};
+
+// TODO: Any additional structs you want to add can go here:
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////// PROVIDED FUNCTION PROTOTYPE  ////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+void print_usage(void);
+void print_carriage(struct carriage *carriage);
+void scan_id(char id_buffer[ID_SIZE]);
+enum carriage_type scan_type(void);
+void print_train_summary(
+    int is_selected, 
+    int n, 
+    int capacity, 
+    int occupancy,
+    int num_carriages
+);
+int compare_double(double n1, double n2);
+
+// Additional provided function prototypes
+// You won't need to use these functions!
+// We use just them to implement some of the provided helper functions.
+int scan_token(char *buffer, int buffer_size);
+char *type_to_string(enum carriage_type type);
+enum carriage_type string_to_type(char *type_str);
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////  YOUR FUNCTION PROTOTYPE  /////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+struct carriage *create_carriage(
+    char id[ID_SIZE], 
+    enum carriage_type type,
+    int capacity
+);
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+int main(void) {
+    printf("Welcome to Carriage Simulator\n");
+    printf("All aboard!\n");
+
+    // TODO: Start stage 1.2 here!
+    struct carriage *test_carriage = create_carriage("N1001", PASSENGER, 20);
+
+    // print out all it's fields.
+    printf("carriage_id: %s\n", test_carriage->carriage_id);
+    printf("capacity: %d\n", test_carriage->capacity);
+    printf("occupancy: %d\n", test_carriage->occupancy);
+    printf("next field: %p\n", test_carriage->next);
+    if (test_carriage->type == PASSENGER) {
+        printf("type: passenger\n");
+    } else {
+        printf("not a passenger carriage\n");
+    }
+
+    printf("\nGoodbye\n");
+
+    return 0;
+}
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////  YOUR FUNCTIONS //////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+
+// STAGE 1.1, creates an empty carraige that isnt linked to the linked list
+struct carriage *create_carriage(
+    char id[ID_SIZE], 
+    enum carriage_type type,
+    int capacity
+) {
+    struct carriage *new_carriage = malloc(sizeof(struct carriage));
+    
+    for (int i = 0; i < ID_SIZE; i++) {
+	    new_carriage->carriage_id[i] = id[i];
+    }
+    new_carriage->type = type;
+    new_carriage->capacity = capacity;    
+    new_carriage->occupancy = 0;
+    new_carriage->next = NULL;
+
+    return new_carriage; 
+}
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+///////////////////////////  PROVIDED FUNCTIONS  ///////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+// Prints the Carriage simulator usage instructions,
+// displaying the different commands and their arguments.
+//
+void print_usage(void) {
+    printf(
+        "=====================[ Carriage Simulator ]=====================\n"
+        "      ===============[     Usage Info     ]===============      \n"
+        "  a [carriage_id] [type] [capacity]                             \n"
+        "    Add a carriage to the train                                 \n"
+        "  p                                                             \n"
+        "    Print out all of the carriages in the train                 \n"
+        "  i [n] [carriage_id] [type] [capacity]                         \n"
+        "    Insert a carriage into the train at position `n`            \n"
+        "                                                                \n"
+        "  s [carriage_id] [n]                                           \n"
+        "    Seat `n` passengers onto the train starting from carriage   \n"
+        "    `carriage_id`                                               \n"
+        "  d [carriage_id] [n]                                           \n"
+        "    Remove `n` passengers from carriage `carriage_id`           \n"
+        "  T                                                             \n"
+        "    Display the total number of passengers and empty seats on   \n"
+        "    the train                                                   \n"
+        "  c [start_id] [end_id]                                         \n"
+        "    Display the number of passengers and empty seats on between \n"
+        "    carriage `start_id` and carriage `end_id`                   \n"
+        "  m [source_id] [destination_id] [n]                            \n"
+        "    Move `n` passengers from one carrige to another, without    \n"
+        "    kicking anyone off the train.                               \n"
+        "  h [carriage_id]                                               \n"
+        "    Display the happiness of passengers in carriage             \n"
+        "    `carriage_id`                                               \n"
+        "  H                                                             \n"
+        "    Display the average happiness of all passengers on the train\n"
+        "                                                                \n"
+        "  N                                                             \n"
+        "    Create a new empty train                                    \n"
+        "  >                                                             \n"
+        "    Select the next train in the train list.                    \n"
+        "  <                                                             \n"
+        "    Select the previous train in the train list.                \n"
+        "  P                                                             \n"
+        "    Display the train list.                                     \n"
+        "  r [carriage_id]                                               \n"
+        "    Remove carriage `carriage_id` from the selected train.      \n"
+        "  R                                                             \n"
+        "    Remove the selected train.                                  \n"
+        "                                                                \n"
+        "  M                                                             \n"
+        "    Merge the selected train with the train after it.           \n"
+        "  S [n]                                                         \n"
+        "    Split the current train into smaller trains.                \n"
+        "  O                                                             \n"
+        "    Rearrange passengers on the selected train to optimise      \n"
+        "    happiness.                                                  \n"
+        "  ?                                                             \n"
+        "    Show help                                                   \n"
+        "================================================================\n"
+    );
+}
+
+
+// Scan in the a carriage id string into the provided buffer, placing 
+// '\0' at the end.
+//
+// Parameters:
+//      id_buffer - a char array of length ID_SIZE, which will be used
+//                  to store the id.
+// 
+// Usage: 
+// ```
+//      char id[ID_SIZE];
+//      scan_id(id);
+// ```
+void scan_id(char id_buffer[ID_SIZE]) {
+    scan_token(id_buffer, ID_SIZE);
+}
+
+
+// Scans a string and converts it to a carriage_type.
+//
+// Returns:
+//      The corresponding carriage_type, if the string was valid,
+//      Otherwise, returns INVALID_TYPE.
+// 
+// Usage: 
+// ```
+//      enum carriage_type type = scan_type();
+// ```
+//
+enum carriage_type scan_type(void) {
+    // This 20 should be #defined, but we've kept it like this to
+    // avoid adding additional constants to your code.
+    char type[20];
+    scan_token(type, 20);
+    return string_to_type(type);
+}
+
+
+// Formats and prints out a train carriage struct,
+//
+// Parameters:
+//      carriage - The struct carriage to print.
+// 
+void print_carriage(struct carriage *carriage) {
+    int line_length = 20;
+
+    char *id = carriage->carriage_id;
+    char *type = type_to_string(carriage->type);
+
+    printf(" ---------\\/--------- \n");
+
+    int padding = line_length - strlen(id);
+    printf("|%*s%s%*s|\n", padding / 2, "", id, (padding + 1) / 2, "");
+
+    padding = line_length - 2 - strlen(type);
+    printf("|%*s(%s)%*s|\n", padding / 2, "", type, (padding + 1) / 2, "");
+
+    printf("| Occupancy: %3d/%-3d |\n", 
+            carriage->occupancy, 
+            carriage->capacity);
+    printf(" ---------||--------- \n");
+}
+
+
+// Formats and prints out various information about a given train.
+//
+// Parameters:
+//      is_selected - 1, if this train is the currently selected train, 
+//                    0, otherwise.
+//      n           - The position of the given train in the list of trains, 
+//                    starting from 0.
+//      capacity    - The total capacity of the given train.
+//      capacity    - The total occupancy of the given train
+//      num_carriages   - The number of carriages in the given train.
+//
+void print_train_summary(
+    int is_selected, 
+    int n, 
+    int capacity, 
+    int occupancy,
+    int num_carriages
+) {
+    if (is_selected) {
+        printf("--->Train #%d\n", n);
+    } else  {
+        printf("    Train #%d\n", n);
+    }
+
+    printf("        Carriages: %3d\n", num_carriages);
+    printf("        Capacity : %3d/%-3d\n", occupancy, capacity);
+    printf("    ----------------------\n");
+
+}
+
+
+
+// Compares two double (floating point) values. Value are considered
+// equal if there is a less than 0.01 difference between them.
+// Note: You should use this function if you need to compare doubles
+//       to eachother, as it reduces inconsistencies caused by double
+//       imprecision.
+//
+// Parameters:
+//      n1 - a floating point value
+//      n2 - a floating point value
+// 
+// Returns:
+//      0, if the two values are considered equal.
+//      a negative number, if n1 is less than n2,
+//      a positive number, if n2 is less than n1,
+//
+// Usage: 
+// ```
+//      if (compare_double(n1, n2) > 0) {
+//          printf("n1 greater than n2\n");
+//      } else if (compare_double(n1, n2) == 0) {
+//          printf("n1 is equal to n2\n");
+//      } else {
+//          printf("n1 is less than n2\n");
+//      }
+// ```
+int compare_double(double n1, double n2) {
+    double delta = 0.01;
+
+    double difference = n1 - n2;
+    // abs(n1 - n2) < delta 
+    if (difference < delta && difference > -delta) {
+        return 0;
+    }
+    if (n1 < n2) {
+        return -1;
+    }
+
+    return 1;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////// ADDITIONAL PROVIDED FUNCTIONS ///////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+// You don't need to use any of these, or understand how they work!
+// We use them to implement some of the provided helper functions.
+
+enum carriage_type string_to_type(char *type_str) {
+    int len = strlen(type_str);
+
+    if (strncasecmp(type_str, "passenger", len) == 0) {
+        return PASSENGER;
+    } 
+    if (strncasecmp(type_str, "buffet", len) == 0) {
+        return BUFFET;
+    } 
+    if (strncasecmp(type_str, "restroom", len) == 0) {
+        return RESTROOM;
+    }
+    if (strncasecmp(type_str, "first_class", len) == 0) {
+        return FIRST_CLASS;
+    } 
+
+    return INVALID_TYPE;
+}
+
+
+char *type_to_string(enum carriage_type type) {
+    if (type == PASSENGER) {
+        return "PASSENGER";
+    } else if (type == BUFFET) {
+        return "BUFFET";
+    } else if (type == FIRST_CLASS) {
+        return "FIRST CLASS";
+    } else if (type == RESTROOM) {
+        return "RESTROOM";
+    }
+
+    return "INVALID";
+}
+
+int scan_token(char *buffer, int buffer_size) {
+    if (buffer_size == 0) {
+        return 0;
+    }
+
+    char c;
+    int i = 0;
+    int num_scanned = 0;
+
+    // consume all leading whitespace
+    scanf(" ");
+
+    // Scan in characters until whitespace
+    while (i < buffer_size - 1
+        && (num_scanned = scanf("%c", &c)) == 1 
+        && !isspace(c)) {
+
+        buffer[i++] = c;
+    }
+
+    if (i > 0) {
+        buffer[i] = '\0';
+    }
+
+    return num_scanned;
+}
